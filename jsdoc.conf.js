7bbc3912560dbcfdd4edb67ca3753681ace47b68
Erhan Koral <erhan.16k@gmail.com>
2023-12-17 12:54:48 +0100

    docdash template added

--- jsdoc.conf.js
+++ jsdoc.conf.js
@@ -1,18 +1,21 @@
-module.exports ={
-    "plugins": [],
-    "recurseDepth": 10,
-    "source": {
-        "include": ["src"],
-        "includePattern": ".js$",
-        "excludePattern": ""
-    },
-    "sourceType": "module",
-    "tags": {
-        "allowUnknownTags": true,
-        "dictionaries": ["jsdoc","closure"]
-    },
-    "templates": {
-        "cleverLinks": false,
-        "monospaceLinks": false
-    }
-}
+module.exports = {
+  plugins: [],
+  recurseDepth: 10,
+  source: {
+    include: ['./src'],
+    includePattern: '.js$',
+    excludePattern: '',
+  },
+  sourceType: 'module',
+  tags: {
+    allowUnknownTags: true,
+    dictionaries: ['jsdoc', 'closure'],
+  },
+  templates: {
+    cleverLinks: false,
+    monospaceLinks: false,
+  },
+  opts: {
+    template: 'node_modules/docdash',
+  },
+};
